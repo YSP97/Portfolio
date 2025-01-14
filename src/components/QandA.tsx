f2c21e1f866210c4149d98b7112c9cb8aeee0f84
YSP97 <pus9717@gmail.com>
2025-01-14 21:21:27 +0900

    [Markup] AboutMe 제작 - QandA 컴포넌트 제작 - notion icon 추가를 위한 sprite 수정 - 예시 이미지 파일 추가

---
+++ src/components/QandA.tsx
@@ -0,0 +1,27 @@
+export default function QandA() {
+  return (
+    <div className="flex flex-col gap-3 text1">
+      <div className="flex gap-3 text-[30px]">
+        <span className="font-semibold font-sora">Q.</span>
+        <span>
+          <span className="font-semibold">개발자</span>가 되려는 이유?
+        </span>
+      </div>
+      <div className="flex items-start gap-5 pl-32">
+        <span className="text-[30px] font-semibold font-sora">A. </span>
+        <p className="text-zinc-500 mt-1">
+          I'm a passionate, self-proclaimed designer who specializes in full
+          stack development (React.js & Node.js). I am very enthusiastic about
+          bringing the technical and visual aspects of digital products to life.
+          User experience, pixel perfect design, and writing clear, readable,
+          highly performant code matters to me.I'm a passionate, self-proclaimed
+          designer who specializes in full stack development (React.js &
+          Node.js). I am very enthusiastic about bringing the technical and
+          visual aspects of digital products to life. User experience, pixel
+          perfect design, and writing clear, readable, highly performant code
+          matters to me.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,7 +1,21 @@
+import QandA from '../components/QandA.tsx';
+
 export default function AboutMe() {
   return (
-    <section id="about-me" className="h-screen bg-gray-100">
-      <h1>About Me</h1>
+    <section id="about-me" className="my-[60px] flex gap-20 px-20 items-center">
+      <img
+        src="public/assets/profile.png"
+        className="max-w-full w-[400px] h-fit shadow-sm"
+      />
+      <div className="flex flex-col gap-5">
+        <div className="flex gap-4 text-[48px] font-sora">
+          <span>About</span>
+          <span className="font-extrabold">Me</span>
+        </div>
+
+        <QandA />
+        <QandA />
+      </div>
     </section>
   );
 }
