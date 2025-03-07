01a9732c93baac074442264a03fa8fc2842fe760
YSP97 <pus9717@gmail.com>
2025-03-08 00:17:11 +0900

    [Feat] react-pdf로 custom 모달창 제작

--- src/components/Pdf.tsx
+++ src/components/Pdf.tsx
@@ -1,17 +1,23 @@
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+
+// PDF.js workerSrc 직접 설정
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.js';
+
 interface PdfProps {
   url?: string;
 }
 
 export default function Pdf({ url }: PdfProps) {
   if (!url) return null;
+
   return (
-    <>
-      <iframe
-        src={url}
-        width="100%"
-        height="600px"
-        style={{ border: 'none' }}
-      ></iframe>
-    </>
+    <div className="w-full h-full overflow-auto border rounded">
+      <Document file={url}>
+        <Page pageNumber={1} width={800} />
+      </Document>
+    </div>
   );
 }
